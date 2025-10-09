d5a5846eda5d91fe2cc6c71f8e58a03756835e0b
Mr-Soul-Forest <stepan.sniff@gmail.com>
2025-10-09 10:51:08 +0300

    alpha0.0 Add data saving for Android
    📱 Добавлено сохранение данных для Android

--- composeApp/src/androidMain/kotlin/forgeofovorldule/anvilory/LoadAndSave.kt
+++ composeApp/src/androidMain/kotlin/forgeofovorldule/anvilory/LoadAndSave.kt
@@ -1,3 +1,5 @@
+package forgeofovorldule.anvilory
+
 /**Copyright 2025 Forge-of-Ovorldule (https://github.com/Forge-of-Ovorldule) and Mr-Soul-Forest (https://github.com/Mr-Soul-Forest)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,7 +12,7 @@
 import android.content.Context
 import android.preference.PreferenceManager
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.encodeToString
+import androidx.core.content.edit
 
 lateinit var appContext: Context
 
@@ -22,5 +24,5 @@ actual fun saveValue(value: Any, name: String) {
         is Boolean -> Json.encodeToString(value)
         else -> Json.encodeToString(value.toString())
     }
-    prefs.edit().putString(name, serialized).apply()
+    prefs.edit { putString(name, serialized) }
 }
